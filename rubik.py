01f50940fee48505a1f1efd2e30e696b3d214a32
marioharison51 <marioharison51@gmail.com>
2026-08-11 19:35:49 +0300

    Update rubik.py

--- rubik.py
+++ rubik.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify, request, render_template
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
-from dotenv import load_dotenv
-import bcrypt
 import ctypes
 import os
+
+import bcrypt
+from dotenv import load_dotenv
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
+from flask_sqlalchemy import SQLAlchemy
 
 # 1. CONFIG
 load_dotenv()
